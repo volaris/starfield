631320d0a07729443b0bad065883260a2257d355
volaris <lhaury@gmail.com>
2015-03-14 13:20:29 -0700

    adding test code

---
+++ controllers/StarfieldDrivers/StarfieldDrivers/Test/SolidColor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using StarfieldClient;
+
+namespace AlgorithmDemo.Drivers
+{
+    public class SolidColor : IStarfieldDriver
+    {
+        #region Private Members
+        Color drawColor = Color.Black;
+        #endregion
+
+        #region Public Properties
+        public Color DrawColor
+        {
+            get { return drawColor; }
+            set { drawColor = value; }
+        }
+        #endregion
+
+        #region IStarfieldDriver Implementation
+        public void Render(StarfieldModel Starfield)
+        {
+            for(ulong x = 0; x < Starfield.NumX; x++)
+            {
+                for (ulong y = 0; y < Starfield.NumY; y++)
+                {
+                    for (ulong z = 0; z < Starfield.NumZ; z++)
+                    {
+                        Starfield.SetColor((int)x, (int)y, (int)z, DrawColor);
+                    }
+                }
+            }
+        }
+
+        void IStarfieldDriver.Start(StarfieldModel Starfield)
+        {
+        }
+
+        void IStarfieldDriver.Stop()
+        {
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return "Solid Color";
+        }
+        #endregion
+    }
+}
