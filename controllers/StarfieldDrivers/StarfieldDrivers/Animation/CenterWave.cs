f2fc6de8476f1be4f4508a42cd9a5425a5179ac6
volaris <lhaury@gmail.com>
2015-04-21 19:48:29 -0700

    Reference Changes
    These changes should allow easier usage when changing between VS and
    Xamarin and different computers. Also checking in a few animations.

---
+++ controllers/StarfieldDrivers/StarfieldDrivers/Animation/CenterWave.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using StarfieldClient;
+using StarfieldUtils.MathUtils;
+using StarfieldUtils.ColorUtils;
+
+namespace AlgorithmDemo.Drivers
+{
+    public class CenterWave : IStarfieldDriver
+    {
+        #region Private Members
+        Color backColor = Color.Black;
+        Color drawColor = Color.Blue;
+        Color[] rainbow10 = new Color[10];
+        Color[] rainbow7 = new Color[7];
+        bool rainbow = true;
+        float interval = 1.3f;
+        ulong step = 0;
+        #endregion
+
+        #region Constructors
+        public CenterWave()
+        {
+            rainbow10[0] = rainbow7[0] = Color.FromArgb(0xFF, 0, 0);
+            rainbow10[1] = rainbow7[1] = Color.FromArgb(0xFF, 0xA5, 0);
+            rainbow10[2] = rainbow7[2] = Color.FromArgb(0xFF, 0xFF, 0);
+            rainbow10[3] = rainbow7[3] = Color.FromArgb(0, 0x80, 0);
+            rainbow10[4] = Color.FromArgb(0, 0xFF, 0);
+            rainbow10[5] = Color.FromArgb(0, 0xA5, 0x80);
+            rainbow10[6] = rainbow7[4] = Color.FromArgb(0, 0, 0xFF);
+            rainbow10[7] = rainbow7[5] = Color.FromArgb(0x4B, 0, 0x82);
+            rainbow10[8] = rainbow7[6] = Color.FromArgb(0xFF, 0, 0xFF);
+            rainbow10[9] = Color.FromArgb(0xEE, 0x82, 0xEE);
+        }
+        #endregion
+
+        #region Public Properties
+        public Color BackColor
+        {
+            get { return backColor; }
+            set { backColor = value; }
+        }
+
+        public Color DrawColor
+        {
+            get { return drawColor; }
+            set { drawColor = value; }
+        }
+
+        public float Interval
+        {
+            get { return interval; }
+            set { interval = value; }
+        }
+
+        public bool Rainbow
+        {
+            get { return rainbow; }
+            set { rainbow = value; }
+        }
+        #endregion
+
+        #region IStarfieldDriver Implementation
+        public void Render(StarfieldModel Starfield)
+        {
+            double distance;
+            Vec3D center = new Vec3D(((double)Starfield.NumX - 1) / 2, 0, ((double)Starfield.NumZ - 1) / 2);
+            Vec3D point;
+            for(ulong x = 0; x < Starfield.NumX; x++)
+            {
+                for (ulong z = 0; z < Starfield.NumZ; z++)
+                {
+                    point = new Vec3D(x, 0, z);
+                    distance = point.DistanceTo(center)/9.899495*8;
+                    double height = (Starfield.NumY / 2) + Math.Sin(distance / interval + (double)step / 50) * (Starfield.NumY / 2);
+
+                    ulong drawY = (ulong)Math.Min(Starfield.NumY - 1, Math.Max(0, Math.Round(height)));
+
+                    for(ulong y = 0; y < Starfield.NumY; y++)
+                    {
+                        Color toDraw = backColor;
+                        if(y == drawY)
+                        {
+                            if (rainbow)
+                            {
+                                toDraw = ColorUtils.GetMultiColorGradient(rainbow7, 1.0f-((float)drawY / (Starfield.NumY-1)), true);
+                            }
+                            else
+                            {
+                                toDraw = drawColor;
+                            }
+                        }
+                        Starfield.SetColor((int)x, (int)y, (int)z, toDraw);
+                    }
+                }
+            }
+            step++;
+        }
+
+        void IStarfieldDriver.Start(StarfieldModel Starfield)
+        {
+            step = 0;
+        }
+
+        void IStarfieldDriver.Stop()
+        {
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return "Center Wave";
+        }
+        #endregion
+    }
+}
---
+++ controllers/StarfieldDrivers/StarfieldDrivers/Sound Responsive/EQBarsVertical.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using StarfieldClient;
+using StarfieldUtils.SoundUtils;
+using StarfieldUtils.ColorUtils;
+
+namespace AlgorithmDemo.Drivers
+{
+    class EQBarsVertical : IStarfieldDriver
+    {
+        #region Private Members
+        int current;
+        int goal;
+        Color[] rainbow10 = new Color[10];
+        Color[] rainbow7 = new Color[7];
+        CSCoreLoopbackSoundProcessor soundProcessor;
+        float maxDistance;
+        bool transitioning = false;
+        float gradientPercent;
+        float gradientStep = .01f;
+        private bool fade = true;
+        private float rate = .8f;
+        Globe vuGlobe = new Globe();
+        #endregion
+
+        #region Public Properties
+        public bool Fade
+        {
+            get { return fade; }
+            set { fade = value; }
+        }
+
+        public float GradientStep
+        {
+            get { return gradientStep; }
+            set { gradientStep = value; }
+        }
+
+        public float Rate
+        {
+            get { return rate; }
+            set { rate = value; }
+        }
+        #endregion
+
+        #region Constructors
+        public EQBarsVertical()
+        {
+            rainbow10[0] = rainbow7[0] = Color.FromArgb(0xFF, 0, 0);
+            rainbow10[1] = rainbow7[1] = Color.FromArgb(0xFF, 0xA5, 0);
+            rainbow10[2] = rainbow7[2] = Color.FromArgb(0xFF, 0xFF, 0);
+            rainbow10[3] = rainbow7[3] = Color.FromArgb(0, 0x80, 0);
+            rainbow10[4] = Color.FromArgb(0, 0xFF, 0);
+            rainbow10[5] = Color.FromArgb(0, 0xA5, 0x80);
+            rainbow10[6] = rainbow7[4] = Color.FromArgb(0, 0, 0xFF);
+            rainbow10[7] = rainbow7[5] = Color.FromArgb(0x4B, 0, 0x82);
+            rainbow10[8] = rainbow7[6] = Color.FromArgb(0xFF, 0, 0xFF);
+            rainbow10[9] = Color.FromArgb(0xEE, 0x82, 0xEE);
+        }
+        #endregion
+
+        #region Event Handlers
+        void soundProcessor_OnFrameUpdate(Frame frame)
+        {
+            byte vu = Math.Max(frame.VU[0], frame.VU[1]);
+            vuGlobe.OuterRadius = 4.0f + ((maxDistance - 4.0f) * (vu / 255f));
+        }
+
+        void soundProcessor_OnArtifactDetected(Artifact artifact)
+        {
+            if (!transitioning)
+            {
+                gradientPercent = 0f;
+                goal = (current + 1) % rainbow7.Length;
+                transitioning = true;
+            }
+        }
+        #endregion
+
+        #region IStarfieldDrive Implementation
+        void IStarfieldDriver.Render(StarfieldModel Starfield)
+        {
+            float centerX = ((Starfield.NumX - 1) * 4.0f) / 2;
+            float centerY = ((Starfield.NumY - 1) * 4.0f) / 2;
+            float centerZ = ((Starfield.NumZ - 1) * 4.0f) / 2;
+
+            maxDistance = (float)Math.Sqrt(Math.Pow(0 - centerX, 2) + Math.Pow(0 - centerY, 2) + Math.Pow(0 - centerZ, 2));
+
+            for (ulong x = 0; x < Starfield.NumX; x++)
+            {
+                for (ulong y = 0; y < Starfield.NumY; y++)
+                {
+                    for (ulong z = 0; z < Starfield.NumZ; z++)
+                    {
+                        float xPos = x * 4.0f;
+                        float yPos = y * 4.0f;
+                        float zPos = z * 4.0f;
+                        Color atPos = Starfield.GetColor((int)x, (int)y, (int)z);
+                        Color toDraw = fade ? Color.FromArgb((int)(rate * atPos.R), (int)(rate * atPos.G), (int)(rate * atPos.B)) : Color.Black;
+
+                        float distanceToCenter = (float)Math.Sqrt(Math.Pow(xPos - centerX, 2) + Math.Pow(yPos - centerY, 2) + Math.Pow(zPos - centerZ, 2));
+
+                        if (distanceToCenter < vuGlobe.OuterRadius && distanceToCenter > vuGlobe.InnerRadius)
+                        {
+                            if (!transitioning)
+                            {
+                                toDraw = rainbow7[current];
+                            }
+                            else
+                            {
+                                if(gradientPercent >= 1f)
+                                {
+                                    current = goal;
+                                    transitioning = false;
+                                }
+                                toDraw = ColorUtils.GetGradientColor(rainbow7[current], rainbow7[goal], gradientPercent, true);
+                                gradientPercent += gradientStep;
+                            }
+                        }
+
+                        Starfield.SetColor((int)x, (int)y, (int)z, toDraw);
+                    }
+                }
+            }
+        }
+
+        void IStarfieldDriver.Start(StarfieldModel Starfield)
+        {
+            soundProcessor = new CSCoreLoopbackSoundProcessor();
+            soundProcessor.ArtifactDelay = 100;
+            soundProcessor.OnArtifactDetected += soundProcessor_OnArtifactDetected;
+            soundProcessor.OnFrameUpdate += soundProcessor_OnFrameUpdate;
+            current = 0;
+            goal = 0;
+            transitioning = false;
+            vuGlobe.InnerRadius = 0;
+        }
+
+        void IStarfieldDriver.Stop()
+        {
+            soundProcessor = null;
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return "EQ Bars - Vertical";
+        }
+        #endregion
+    }
+}
