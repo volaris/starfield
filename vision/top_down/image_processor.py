e98b424728b9805aaccbb6ea7766e5a438e62595
volaris <lhaury@gmail.com>
2016-03-26 18:13:40 -0700

    Adding image_processor.py
    This script analyzes the blended image and creates an activity index for
    each pixel.

---
+++ vision/top_down/image_processor.py
@@ -0,0 +1,44 @@
+import json
+from PIL import Image
+
+configFile = "./config/processor.json"
+outDir = "./output/"
+
+config = None
+
+def loadConfig():
+    global config
+    with open(configFile) as data_file:
+        config = json.load(data_file)
+        
+def loadImage():
+    image = Image.open(outDir + "out.jpg").copy().convert("RGBA")
+    return image
+    
+def processImage(image):
+    activity = [[0 for x in range(11)] for x in range(11)]
+    base = (config["origin"]["x"], config["origin"]["y"])
+    step = config["pixel_step"]
+    width = config["pixel_width"]
+    for x in range(11):
+        for y in range(11):
+            point = (base[0] + x * step, base[1] + y * step)
+            for pixX in range(int(point[0] - width), int(point[0] + width)):
+                for pixY in range(int(point[1] - width), int(point[1] + width)):
+                    pixel = image.getpixel((pixX, pixY))
+                    val = ((pixel[0] + pixel[1] + pixel[2]) / 3) / 255.0
+                    activity[x][y] = activity[x][y] + val
+    return activity
+    
+def exportActivity(activity):
+    points = []
+    for x in range(11):
+        for y in range(11):
+            points.append({"x" : x, "y" : y, "activity" : activity[x][y]})
+    with open(outDir + "activity.json", "w+") as out_file:
+        json.dump(points, out_file, indent=2)
+    
+loadConfig()
+img = loadImage()
+act = processImage(img)
+exportActivity(act)
