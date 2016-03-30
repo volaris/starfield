034fd6d71a380d3156546f0985fcc3557314dcc3
volaris <lhaury@gmail.com>
2016-03-29 23:14:13 -0700

    Adding vision system demo viewer

---
+++ vision/ActivityDemo/ActivityDemo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ActivityDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDraw = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelDraw
+            // 
+            this.panelDraw.Location = new System.Drawing.Point(12, 12);
+            this.panelDraw.Name = "panelDraw";
+            this.panelDraw.Size = new System.Drawing.Size(225, 225);
+            this.panelDraw.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 249);
+            this.Controls.Add(this.panelDraw);
+            this.MaximumSize = new System.Drawing.Size(265, 288);
+            this.MinimumSize = new System.Drawing.Size(265, 288);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDraw;
+    }
+}
+
---
+++ vision/ActivityDemo/ActivityDemo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ActivityDemo
+{
+    public class Activity
+    {
+        public double activity;
+    }
+
+    public partial class Form1 : Form
+    {
+        string path;
+        Timer myTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            myTimer.Tick += myTimer_Tick;
+        }
+
+        void myTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(path))
+                {
+                    string json = r.ReadToEnd();
+                    List<List<Activity>> foo = JsonConvert.DeserializeObject<List<List<Activity>>>(json);
+                    Color baseColor = Color.Red;
+
+                    for (int x = 0; x < foo.Count; x++)
+                    {
+                        for (int y = 0; y < foo[x].Count; y++)
+                        {
+                            int pixX = x * 15 + (int)(x * 5.5);
+                            int pixY = y * 15 + (int)(y * 5.5);
+
+                            double pct = foo[x][y].activity / 100;
+                            Color draw = Color.FromArgb((int)(baseColor.R * pct), (int)(baseColor.G * pct), (int)(baseColor.B * pct));
+                            Brush pen = new SolidBrush(draw);
+
+                            panelDraw.CreateGraphics().FillRectangle(pen, new Rectangle(pixX, pixY, 15, 15));
+                        }
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            OpenFileDialog myOFD = new OpenFileDialog();
+            if (myOFD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = myOFD.FileName;
+                myTimer.Interval = 100;
+                myTimer.Enabled = true;
+            }
+        }
+    }
+}
