bdc36584b71fdd176782fb7889e575b61db08f65
Lane Haury <lhaury@gmail.com>
2016-09-21 23:10:25 -0700

    Ambient controller type filtering

--- controllers/Ambient/Ambient/FormDemo.Designer.cs
+++ controllers/Ambient/Ambient/FormDemo.Designer.cs
@@ -41,6 +41,11 @@
             this.buttonReconnect = new System.Windows.Forms.Button();
             this.textBoxAlgorithm = new System.Windows.Forms.TextBox();
             this.buttonNext = new System.Windows.Forms.Button();
+            this.checkBoxSoundResponsive = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmbientInteractive = new System.Windows.Forms.CheckBox();
+            this.buttonReload = new System.Windows.Forms.Button();
+            this.checkBoxInteractive = new System.Windows.Forms.CheckBox();
+            this.checkBoxAmbient = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarBrightness)).BeginInit();
             this.SuspendLayout();
@@ -48,9 +53,10 @@
             // labelAlgorithm
             // 
             this.labelAlgorithm.AutoSize = true;
-            this.labelAlgorithm.Location = new System.Drawing.Point(12, 43);
+            this.labelAlgorithm.Location = new System.Drawing.Point(24, 83);
+            this.labelAlgorithm.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelAlgorithm.Name = "labelAlgorithm";
-            this.labelAlgorithm.Size = new System.Drawing.Size(50, 13);
+            this.labelAlgorithm.Size = new System.Drawing.Size(102, 25);
             this.labelAlgorithm.TabIndex = 0;
             this.labelAlgorithm.Text = "Algorithm";
             // 
@@ -58,9 +64,10 @@
             // 
             this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar1.Location = new System.Drawing.Point(94, 67);
+            this.trackBar1.Location = new System.Drawing.Point(188, 129);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(6);
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(388, 45);
+            this.trackBar1.Size = new System.Drawing.Size(776, 90);
             this.trackBar1.TabIndex = 2;
             this.trackBar1.Value = 10;
             this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
@@ -68,59 +75,66 @@
             // labelRenderSpeed
             // 
             this.labelRenderSpeed.AutoSize = true;
-            this.labelRenderSpeed.Location = new System.Drawing.Point(12, 76);
+            this.labelRenderSpeed.Location = new System.Drawing.Point(24, 146);
+            this.labelRenderSpeed.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelRenderSpeed.Name = "labelRenderSpeed";
-            this.labelRenderSpeed.Size = new System.Drawing.Size(76, 13);
+            this.labelRenderSpeed.Size = new System.Drawing.Size(150, 25);
             this.labelRenderSpeed.TabIndex = 3;
             this.labelRenderSpeed.Text = "Render Speed";
             // 
             // labelStarfield
             // 
             this.labelStarfield.AutoSize = true;
-            this.labelStarfield.Location = new System.Drawing.Point(12, 16);
+            this.labelStarfield.Location = new System.Drawing.Point(24, 31);
+            this.labelStarfield.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelStarfield.Name = "labelStarfield";
-            this.labelStarfield.Size = new System.Drawing.Size(45, 13);
+            this.labelStarfield.Size = new System.Drawing.Size(91, 25);
             this.labelStarfield.TabIndex = 4;
             this.labelStarfield.Text = "Starfield";
             // 
             // comboBoxStarfield
             // 
             this.comboBoxStarfield.FormattingEnabled = true;
-            this.comboBoxStarfield.Location = new System.Drawing.Point(63, 13);
+            this.comboBoxStarfield.Location = new System.Drawing.Point(126, 25);
+            this.comboBoxStarfield.Margin = new System.Windows.Forms.Padding(6);
             this.comboBoxStarfield.Name = "comboBoxStarfield";
-            this.comboBoxStarfield.Size = new System.Drawing.Size(193, 21);
+            this.comboBoxStarfield.Size = new System.Drawing.Size(382, 33);
             this.comboBoxStarfield.TabIndex = 5;
             this.comboBoxStarfield.SelectedIndexChanged += new System.EventHandler(this.comboBoxStarfield_SelectedIndexChanged);
             // 
             // textBoxIP
             // 
-            this.textBoxIP.Location = new System.Drawing.Point(262, 13);
+            this.textBoxIP.Location = new System.Drawing.Point(524, 25);
+            this.textBoxIP.Margin = new System.Windows.Forms.Padding(6);
             this.textBoxIP.Name = "textBoxIP";
-            this.textBoxIP.Size = new System.Drawing.Size(84, 20);
+            this.textBoxIP.Size = new System.Drawing.Size(164, 31);
             this.textBoxIP.TabIndex = 7;
             // 
             // textBoxPort
             // 
-            this.textBoxPort.Location = new System.Drawing.Point(368, 13);
+            this.textBoxPort.Location = new System.Drawing.Point(736, 25);
+            this.textBoxPort.Margin = new System.Windows.Forms.Padding(6);
             this.textBoxPort.Name = "textBoxPort";
-            this.textBoxPort.Size = new System.Drawing.Size(33, 20);
+            this.textBoxPort.Size = new System.Drawing.Size(62, 31);
             this.textBoxPort.TabIndex = 8;
             // 
             // labelEndpoint
             // 
             this.labelEndpoint.AutoSize = true;
-            this.labelEndpoint.Location = new System.Drawing.Point(352, 17);
+            this.labelEndpoint.Location = new System.Drawing.Point(704, 33);
+            this.labelEndpoint.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelEndpoint.Name = "labelEndpoint";
-            this.labelEndpoint.Size = new System.Drawing.Size(10, 13);
+            this.labelEndpoint.Size = new System.Drawing.Size(18, 25);
             this.labelEndpoint.TabIndex = 9;
             this.labelEndpoint.Text = ":";
             // 
             // labelBrightness
             // 
             this.labelBrightness.AutoSize = true;
-            this.labelBrightness.Location = new System.Drawing.Point(12, 127);
+            this.labelBrightness.Location = new System.Drawing.Point(24, 244);
+            this.labelBrightness.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.labelBrightness.Name = "labelBrightness";
-            this.labelBrightness.Size = new System.Drawing.Size(56, 13);
+            this.labelBrightness.Size = new System.Drawing.Size(114, 25);
             this.labelBrightness.TabIndex = 11;
             this.labelBrightness.Text = "Brightness";
             // 
@@ -128,19 +142,21 @@
             // 
             this.trackBarBrightness.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBarBrightness.Location = new System.Drawing.Point(74, 118);
+            this.trackBarBrightness.Location = new System.Drawing.Point(148, 227);
+            this.trackBarBrightness.Margin = new System.Windows.Forms.Padding(6);
             this.trackBarBrightness.Maximum = 100;
             this.trackBarBrightness.Name = "trackBarBrightness";
-            this.trackBarBrightness.Size = new System.Drawing.Size(408, 45);
+            this.trackBarBrightness.Size = new System.Drawing.Size(816, 90);
             this.trackBarBrightness.TabIndex = 12;
             this.trackBarBrightness.Value = 100;
             this.trackBarBrightness.ValueChanged += new System.EventHandler(this.trackBarBrightness_ValueChanged);
             // 
             // buttonReconnect
             // 
-            this.buttonReconnect.Location = new System.Drawing.Point(407, 11);
+            this.buttonReconnect.Location = new System.Drawing.Point(814, 21);
+            this.buttonReconnect.Margin = new System.Windows.Forms.Padding(6);
             this.buttonReconnect.Name = "buttonReconnect";
-            this.buttonReconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonReconnect.Size = new System.Drawing.Size(150, 44);
             this.buttonReconnect.TabIndex = 13;
             this.buttonReconnect.Text = "Reconnect";
             this.buttonReconnect.UseVisualStyleBackColor = true;
@@ -148,27 +164,86 @@
             // 
             // textBoxAlgorithm
             // 
-            this.textBoxAlgorithm.Location = new System.Drawing.Point(63, 41);
+            this.textBoxAlgorithm.Location = new System.Drawing.Point(126, 79);
+            this.textBoxAlgorithm.Margin = new System.Windows.Forms.Padding(6);
             this.textBoxAlgorithm.Name = "textBoxAlgorithm";
             this.textBoxAlgorithm.ReadOnly = true;
-            this.textBoxAlgorithm.Size = new System.Drawing.Size(338, 20);
+            this.textBoxAlgorithm.Size = new System.Drawing.Size(672, 31);
             this.textBoxAlgorithm.TabIndex = 14;
             // 
             // buttonNext
             // 
-            this.buttonNext.Location = new System.Drawing.Point(407, 39);
+            this.buttonNext.Location = new System.Drawing.Point(814, 75);
+            this.buttonNext.Margin = new System.Windows.Forms.Padding(6);
             this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.Size = new System.Drawing.Size(150, 44);
             this.buttonNext.TabIndex = 15;
             this.buttonNext.Text = "Next";
             this.buttonNext.UseVisualStyleBackColor = true;
             this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
             // 
+            // checkBoxSoundResponsive
+            // 
+            this.checkBoxSoundResponsive.AutoSize = true;
+            this.checkBoxSoundResponsive.Location = new System.Drawing.Point(29, 304);
+            this.checkBoxSoundResponsive.Name = "checkBoxSoundResponsive";
+            this.checkBoxSoundResponsive.Size = new System.Drawing.Size(225, 29);
+            this.checkBoxSoundResponsive.TabIndex = 16;
+            this.checkBoxSoundResponsive.Text = "Sound Responsive";
+            this.checkBoxSoundResponsive.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmbientInteractive
+            // 
+            this.checkBoxAmbientInteractive.AutoSize = true;
+            this.checkBoxAmbientInteractive.Location = new System.Drawing.Point(29, 339);
+            this.checkBoxAmbientInteractive.Name = "checkBoxAmbientInteractive";
+            this.checkBoxAmbientInteractive.Size = new System.Drawing.Size(227, 29);
+            this.checkBoxAmbientInteractive.TabIndex = 17;
+            this.checkBoxAmbientInteractive.Text = "Ambient Interactive";
+            this.checkBoxAmbientInteractive.UseVisualStyleBackColor = true;
+            // 
+            // buttonReload
+            // 
+            this.buttonReload.Location = new System.Drawing.Point(442, 304);
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.Size = new System.Drawing.Size(180, 64);
+            this.buttonReload.TabIndex = 18;
+            this.buttonReload.Text = "Reload Drivers";
+            this.buttonReload.UseVisualStyleBackColor = true;
+            this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // checkBoxInteractive
+            // 
+            this.checkBoxInteractive.AutoSize = true;
+            this.checkBoxInteractive.Location = new System.Drawing.Point(281, 304);
+            this.checkBoxInteractive.Name = "checkBoxInteractive";
+            this.checkBoxInteractive.Size = new System.Drawing.Size(143, 29);
+            this.checkBoxInteractive.TabIndex = 19;
+            this.checkBoxInteractive.Text = "Interactive";
+            this.checkBoxInteractive.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxAmbient
+            // 
+            this.checkBoxAmbient.AutoSize = true;
+            this.checkBoxAmbient.Checked = true;
+            this.checkBoxAmbient.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAmbient.Location = new System.Drawing.Point(281, 339);
+            this.checkBoxAmbient.Name = "checkBoxAmbient";
+            this.checkBoxAmbient.Size = new System.Drawing.Size(122, 29);
+            this.checkBoxAmbient.TabIndex = 20;
+            this.checkBoxAmbient.Text = "Ambient";
+            this.checkBoxAmbient.UseVisualStyleBackColor = true;
+            // 
             // FormDemo
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 166);
+            this.ClientSize = new System.Drawing.Size(988, 396);
+            this.Controls.Add(this.checkBoxAmbient);
+            this.Controls.Add(this.checkBoxInteractive);
+            this.Controls.Add(this.buttonReload);
+            this.Controls.Add(this.checkBoxAmbientInteractive);
+            this.Controls.Add(this.checkBoxSoundResponsive);
             this.Controls.Add(this.buttonNext);
             this.Controls.Add(this.textBoxAlgorithm);
             this.Controls.Add(this.buttonReconnect);
@@ -182,6 +257,7 @@
             this.Controls.Add(this.labelRenderSpeed);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.labelAlgorithm);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "FormDemo";
             this.Text = "Starfield Display Driver";
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
@@ -206,6 +282,11 @@
         private System.Windows.Forms.Button buttonReconnect;
         private System.Windows.Forms.TextBox textBoxAlgorithm;
         private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.CheckBox checkBoxSoundResponsive;
+        private System.Windows.Forms.CheckBox checkBoxAmbientInteractive;
+        private System.Windows.Forms.Button buttonReload;
+        private System.Windows.Forms.CheckBox checkBoxInteractive;
+        private System.Windows.Forms.CheckBox checkBoxAmbient;
     }
 }
 
