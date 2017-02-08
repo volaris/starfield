36f398e1da9fd64fca0971818a42f09fe45e1712
volaris <lhaury@gmail.com>
2017-02-08 00:24:50 -0800

    Documentation Generation

--- lib/.NET/StarfieldClient/StarfieldClient/IStarfieldDriver.cs
+++ lib/.NET/StarfieldClient/StarfieldClient/IStarfieldDriver.cs
@@ -3,21 +3,37 @@ using Starfield;
 
 namespace Starfield
 {
+    /**
+     * <summary>    Interface for starfield drivers. </summary>
+     */
+
     public interface IStarfieldDriver
     {
-        // called at every render iteration
+        /**
+         * <summary>    Renders the given Starfield. Called at every render iteration.</summary>
+         *
+         * <param name="Starfield"> The starfield model to render to. </param>
+         */
         void Render(StarfieldModel Starfield);
 
-        // called to initialize the driver when it is selected
-        // use the constructor to initialize state that will remain initialized
-        // for the life of the client, use this initialize state that should
-        // only be initialized while this driver is running or needs to be
-        // reset when this driver takes over
+        /**
+         * <summary>    Starts the starfield driver. </summary>
+         * <para>    Called to initialize the driver when it is selected 
+         *           use the constructor to initialize state that will 
+         *           remain initialized for the life of the client, use 
+         *           this initialize state that should only be 
+         *           initialized while this driver is running or needs 
+         *           to be reset when this driver takes over</para>
+         *
+         * <param name="Starfield"> The starfield. </param>
+         */
         void Start(StarfieldModel Starfield);
 
-        // called when another driver is selected
-        // clean up anything that should not be running while another driver
-        // is controlling the starfield
+        /** <summary>    Stops the starfield driver. </summary>  
+         *  <para>    Called when another driver is selected. Clean up 
+         *            anything that should not be running while another 
+         *            driver is controlling the starfield</para>
+         */
         void Stop();
     }
 
